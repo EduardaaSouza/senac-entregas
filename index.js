da9a0ab04bfce93fb2a1da4423ae35a19b0a4d66
EduardaaSouza <166620200+EduardaaSouza@users.noreply.github.com>
2024-04-16 11:56:00 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let nome 
+let idade 
+
+console.log("tipo do nome", typeof nome)
+console.log("tipo da idade", typeof idade)
+\\aparce indefinido pq nao foi atribuido nenhum valor
+\\colocar o sinal de igual para atribuir o valor 
+
+let nome = jose
+let idade = 50
+
+console.log("qual seu nome", prompt nome)
+console.log("qual sua idade", prompt idade)
+
+let nome = jose
+let idade= 50
+
+console.log("qual seu nome",typeof nome)
+console.log("qual sua idade", typeof idade)
+\\variavel errada
+\\ primira tentiva modo de digitar errado
+
+nome = prompt("qual seu nome")
+idade = prompt("qual seu idade")
+
+console.log("tipo da variavel nome",typeof nome)
+console.log("tipo da variavel idade", typeof idade)
